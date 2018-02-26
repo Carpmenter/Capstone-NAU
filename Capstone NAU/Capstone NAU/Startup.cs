518d451c264b15b8287333f090a4ca2d64893487
Carpmenter <NickCarpenter1017@gmail.com>
2018-02-26 17:49:17 -0600

    Database Classes created and implemented

--- Capstone NAU/Capstone NAU/Startup.cs
+++ Capstone NAU/Capstone NAU/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-//using Capstone_NAU.Models;
+using Capstone_NAU.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Capstone_NAU
@@ -23,8 +23,8 @@ namespace Capstone_NAU
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<QuestionContext>(options =>
-           // options.UseSqlServer(Configuration.GetConnectionString("QuestionContext")));
+            services.AddDbContext<NAUcountryContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("NAUContext")));
             services.AddMvc();
         }
 
